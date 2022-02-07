14839e84e56de3ebe57eb25bad7ee0a8966f5a46
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-02-07 20:37:36 +0700

    Fixing Top Bar Admin Page

--- resources/views/admin/layouts/topbar.blade.php
+++ resources/views/admin/layouts/topbar.blade.php
@@ -4,11 +4,8 @@
     <div class="mobile-toggle-icon d-xl-none">
       <i class="bi bi-list"></i>
     </div>
-    <div class="top-navbar d-none d-xl-block">
+    <div class="top-navbar">
       <ul class="navbar-nav align-items-center">
-        <li class="nav-item">
-          <a class="nav-link" href="/admin">Dashboard</a>
-        </li>
         <li class="nav-item">
           <a class="nav-link" href="/">Main Website</a>
         </li>
